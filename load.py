27503b786d59450b22baaa3b29c176b1aca1db32
jakebobu <jakebobula@gmail.com>
2017-10-10 23:09:03 -0600

    I am not completely sure this is how this should start, but this is what I have so far.

--- load.py
+++ load.py
@@ -1,1 +1,12 @@
-import tweepy as t
+import tweepy
+from tweepy import OAuthHandler
+
+consumer_key = "0XlgcKe6UTnXiSyikruP3upFw"
+consumer_secret = "gwHgg8ds7idiEZrwuKtFrUQHEBUqI0VkEY0nZah46cjCTf5Shc"
+access_token = "3890310195-DKYpqXORud5MOR2zicsscgAyCAH5K4NOUN6SEqJ"
+access_secret =  "26yMtFG6bQudSF9svnAz0L4h7Ah5T2SmyJZ1Jx2Jt5pLx"
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
