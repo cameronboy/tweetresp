5772c382b61d82db1561a6bfdf1254f11f2133a2
Cameron Boy <camerondavidboy@gmail.com>
2017-10-12 08:29:17 -0600

    Removal of OAuthHandler import
    By importing tweepy, we are implicitly importing all of its methods. So we can access the OAuthHandler by putting tweepy. in front of it
    
    I believe we could also remove line 3, as textblob is already imported :)

--- messups.py
+++ messups.py
@@ -1,14 +1,14 @@
 import tweepy
 import textblob
 from textblob import TextBlob
-from tweepy import OAuthHandler
+
 
 consumer_key = "0XlgcKe6UTnXiSyikruP3upFw"
 consumer_secret = "gwHgg8ds7idiEZrwuKtFrUQHEBUqI0VkEY0nZah46cjCTf5Shc"
 access_token = "3890310195-DKYpqXORud5MOR2zicsscgAyCAH5K4NOUN6SEqJ"
 access_secret =  "26yMtFG6bQudSF9svnAz0L4h7Ah5T2SmyJZ1Jx2Jt5pLx"
 
-auth = OAuthHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
