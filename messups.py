61d5d2c1d091d078ba291a74b2750f9945787d79
Cameron Boy <camerondavidboy@gmail.com>
2017-10-12 09:56:31 -0600

    refactored to pull credentials from json objects

--- messups.py
+++ messups.py
@@ -1,12 +1,23 @@
 import tweepy
-import textblob
-from textblob import TextBlob
+import textblob as tb
+import json
 
+# Load credentials into variables
+"""
+Let's store the credentials in a json object, that way we can reference that without having to hardcode
+our twitter app credentials in the code :)
+"""
 
-consumer_key = "0XlgcKe6UTnXiSyikruP3upFw"
-consumer_secret = "gwHgg8ds7idiEZrwuKtFrUQHEBUqI0VkEY0nZah46cjCTf5Shc"
-access_token = "3890310195-DKYpqXORud5MOR2zicsscgAyCAH5K4NOUN6SEqJ"
-access_secret =  "26yMtFG6bQudSF9svnAz0L4h7Ah5T2SmyJZ1Jx2Jt5pLx"
+with open('cred.json') as f:
+    # Load the credentials file
+    cred = json.load(f)
+    # assign the objects to variables
+    consumer_key = cred["consumer_key"]
+    consumer_secret = cred["consumer_secret"]
+    access_token = cred["access_token"]
+    access_secret = cred["access_secret"]
+#close the file
+f.close()
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
@@ -21,5 +32,5 @@ print search_term
 
 for tweet in public_tweets:
     print tweet.text
-    analysis = TextBlob(tweet.text)
+    analysis = tb.TextBlob(tweet.text)
     print analysis.sentiment
