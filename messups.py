3342806d5815af83102fc46a51a59049ebb04bcf
jakebobu <jakebobula@gmail.com>
2017-10-14 19:33:49 -0600

    added comments

--- messups.py
+++ messups.py
@@ -19,24 +19,32 @@ with open('cred.json') as f:
 #close the file
 f.close()
 
+#passing credentials to access twitter and assign it to api variable
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
-
 api = tweepy.API(auth)
 
+#in the future search_term will be an input
 search_term = 'football'
 
+#assigning all the relevant tweets to public_tweets
 public_tweets = api.search(search_term)
 
+#all of these print commands are just to see a read out in terminal to
+#understand what the program is doing
 print(search_term)
 
 for tweet in public_tweets:
-    print(tweet.text)
+    #this is an analysis using a module called textblob
     analysis = tb.TextBlob(tweet.text)
+
+    print(tweet.text)
     print(analysis.sentiment)
+
     #it seems like there are four attributes that are relavent to mapping tweets
     #the problem is most in practice are null, user.location often has something
-    #but it is not necessary to use a real answer, "home sweet home" is hard to map :)
+    #but it is not necessary to use a real answer
+    #"home sweet home" is hard to map :)
     print(tweet.user.location)
     print(tweet.coordinates)
     print(tweet.geo)
