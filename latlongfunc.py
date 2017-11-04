bbb2e924d34a24afdc777e5429fe2d5a8b293c55
jakebobu <jakebobula@gmail.com>
2017-11-04 14:21:41 -0600

    changed the lalong script into a function

---
+++ latlongfunc.py
@@ -0,0 +1,33 @@
+# Using Python requests and the Google Maps Geocoding API.
+#
+# from https://gist.github.com/pnavarrc/5379521
+#
+# References:
+#
+# * http://docs.python-requests.org/en/latest/
+# * https://developers.google.com/maps/
+
+import requests
+
+def latlong (term):
+
+    GOOGLE_MAPS_API_URL = 'http://maps.googleapis.com/maps/api/geocode/json'
+
+    params = {
+        'address': term,
+        'sensor': 'false',
+    }
+
+    # Do the request and get the response data
+    req = requests.get(GOOGLE_MAPS_API_URL, params=params)
+    res = req.json()
+
+    # Use the first result
+    result = res['results'][0]
+
+    geodata = dict()
+    geodata['lat'] = result['geometry']['location']['lat']
+    geodata['lng'] = result['geometry']['location']['lng']
+    geodata['address'] = result['formatted_address']
+
+    return '({lat}, {lng})'.format(**geodata)
